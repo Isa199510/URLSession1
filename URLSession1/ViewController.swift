c4d1481419e48e9e7f0f11c3476abf3c722bb4db
Isa199510 <isa.arbuhov@mail.ru>
2022-11-18 23:41:39 +0600

    Initial Commit

---
+++ URLSession1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLSession1
+//
+//  Created by Иса on 18.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
